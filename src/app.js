61c1760d91ba6b34ce7452e03380c67ea2471c10
Nicolas Quintero <nicolasquintero151@gmail.com>
2022-07-20 21:40:22 -0500

    cambio v3

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const {resolve} = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{
-    console.log("servidorpuerto2020")
-});
-const public = resolve(__dirname, '../public');
-const static = express.static(public);
-app.use(static);
+
+app.use('/', express.static(__dirname + '../public'))
+
 
 app.get('/', (req, res) => {
   let view = resolve(__dirname, '../views','index.html');
   return res.sendFile(view);
 }) ;
+app.listen(port,()=>{
+    console.log("servidorpuerto2020")
+});
