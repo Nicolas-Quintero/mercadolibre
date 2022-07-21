7d15e29bf54fcee86f83c12bcb60e21d2a3905cd
Nicolas Quintero <nicolasquintero151@gmail.com>
2022-07-20 21:21:44 -0500

    cambio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {resolve} = require('path');
 const app = express();
-const port = process.env.PORT || 2020;
+const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log("servidorpuerto2020")
 });
