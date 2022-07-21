52eb88de08b1ed9cbce12bc480d9368efa289515
Nicolas Quintero <nicolasquintero151@gmail.com>
2022-07-20 21:47:43 -0500

    cambio v5

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ const {resolve} = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '../public'))
-
+app.use('/', express.static(__dirname + '/public'))
+console.log(__dirname);
 
 app.get('/', (req, res) => {
   let view = resolve(__dirname, '../views','index.html');
   return res.sendFile(view);
 }) ;
 app.listen(port,()=>{
-    console.log("servidorpuerto2020")
+    console.log("servidor puerto 3000")
 });
