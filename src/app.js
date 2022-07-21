50e7a2b8b0264321435fe83b739d4fe2322ecd70
Nicolas Quintero <nicolasquintero151@gmail.com>
2022-07-20 21:57:44 -0500

    cambio v8

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const {resolve} = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+app.set('puerto', process.env.PORT || 3000)
 
 app.use('/', express.static(__dirname + '/public'))
 console.log(__dirname);
 
 app.get('/', (req, res) => {
-  let view = resolve(__dirname, '/views','index.html');
+  let view = resolve(__dirname, 'views','index.html');
   return res.sendFile(view);
 }) ;
-app.listen(port,()=>{
-    console.log("servidor puerto 3000")
+app.listen(app.get('puerto'),()=>{
+    console.log(`servidor puerto ${app.get('puerto')}`)
 });
